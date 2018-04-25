93b56df2c8e7a6decf2db7072cb6c7aca89e0313
Hector Romojaro <hector.romojaro@gmail.com>
2018-04-25 19:47:46 +0000

    Prefer '{}' to '[list]' when creating empty lists

--- tcl/chat-procs.tcl
+++ tcl/chat-procs.tcl
@@ -634,7 +634,7 @@ ad_proc -private chat_room_flush {
         # do we have to create a transcript for the room
         if { $room_info(auto_transcript_p) == "t" } {
             # build a list of all messages
-	    set contents [list]
+	    set contents {}
             foreach message [db_list_of_lists get_archives_messages {}] {
 		lassign $message msg creation_user creation_date
                 set user_name [expr {$creation_user > 0 ? [chat_user_name $creation_user] : "system"}]
