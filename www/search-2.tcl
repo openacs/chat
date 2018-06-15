be140b14d84b9760523863045ed72a3ce1b526da
Hector Romojaro <hector.romojaro@gmail.com>
2018-06-15 23:11:09 +0000

    Whitespace changes

--- www/search-2.tcl
+++ www/search-2.tcl
@@ -28,7 +28,7 @@ set SQL_LIMIT 20
 set context [list "Search"]
 
 if {[info exists keyword]} {
-    # this is an administrator 
+    # this is an administrator
     if { $keyword eq "" } {
         incr exception_count
         append exception_text "<li>You forgot to type a search string!\n"
@@ -67,11 +67,11 @@ if { [info exists keyword] } {
     set sql_keyword "%[string tolower $keyword]%"
     lappend where_clause "(username like :sql_keyword or email like :sql_keyword or lower(first_names || ' ' || last_name) like :sql_keyword)"
 } elseif { [info exists email] && $email ne "" } {
-    set search_type "email"    
+    set search_type "email"
     set sql_email "%[string tolower $email]%"
     lappend where_clause "email like :sql_email"
 } else {
-    set search_type "last"        
+    set search_type "last"
     set sql_last_name "%[string tolower $last_name]%"
     lappend where_clause "lower(last_name) like :sql_last_name"
 }
@@ -113,7 +113,7 @@ db_foreach user_search_admin $query {
     set first_names_from_search $first_names
     set last_name_from_search $last_name
     set email_from_search $email
-    
+
     set user_search:[set rowcount](user_id) $user_id
     set user_search:[set rowcount](first_names) $first_names
     set user_search:[set rowcount](last_name) $last_name
@@ -122,7 +122,7 @@ db_foreach user_search_admin $query {
     set user_search:[set rowcount](member_state) $member_state
     set user_search:[set rowcount](url) [export_vars -base "search-3" {room_id type {party_id $user_id}}]
 
-    
+
     if { $member_state ne "approved" } {
         set user_search:[set rowcount](user_finite_state_links) [join [ad_registration_finite_state_machine_admin_links $member_state $email_verified_p $user_id_from_search [export_vars -base search {email last_name keyword target passthrough limit_users_in_group_id only_authorized_p}]] " | "]
     } else {
