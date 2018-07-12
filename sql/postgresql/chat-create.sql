29ef1b154227c42d2d8197dbd11c016307664a69
Antonio Pisano <antonio@elettrotecnica.it>
2018-07-12 12:43:09 +0000

    Align case of acs_object_type with that of the actual table (fixes automated tests)
    This change intentionally ignores Oracle, as its behavior with respect to case is not clear to me.

--- sql/postgresql/chat-create.sql
+++ sql/postgresql/chat-create.sql
@@ -76,8 +76,8 @@ AS 'declare
           ''Chat Room'',  -- pretty_name
           ''Chat Rooms'', -- pretty_plural
           ''acs_object'', -- supertype
-          ''CHAT_ROOMS'', -- table_name
-          ''ROOM_ID'',    -- id_column
+          ''chat_rooms'', -- table_name
+          ''room_id'',    -- id_column
           null,           -- package_name
           ''f'',          -- abstract_p
           null,           -- type_extension_table
@@ -229,8 +229,8 @@ AS 'declare
           ''Chat Transcript'',  -- pretty_name
           ''Chat Transcripts'', -- pretty_plural
           ''acs_object'',       -- supertype
-          ''CHAT_TRANSCRIPTS'', -- table_name
-          ''TRANSCRIPT_ID'',    -- id_column
+          ''chat_transcripts'', -- table_name
+          ''transcript_id'',    -- id_column
           null,           -- package_name
           ''f'',          -- abstract_p
           null,           -- type_extension_table
---
+++ sql/postgresql/upgrade/upgrade-5.0-5.0.1d0.sql
@@ -0,0 +1,8 @@
+
+DO $$
+BEGIN
+   update acs_object_types set
+      table_name = lower(table_name),
+      id_column = lower(id_column)
+    where object_type in ('chat_room', 'chat_transcript');
+END$$;
