9042e2f7dc402eee37febde0986d64904fa9a00d
Antonio Pisano <antonio@elettrotecnica.it>
2018-09-18 14:44:42 +0000

    Decouple a bit formatting from data

---
+++ www/ajax/chat.adp
@@ -0,0 +1,48 @@
+<html>
+  <style type='text/css'>
+      #messages {
+          font-size: 12px;
+          color: #666666;
+          font-family: Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Arial, sans-serif;
+      }
+      #messages .timestamp {
+          vertical-align: top;
+          color: #CCCCCC;
+      }
+      #messages .user {
+          margin: 0px 5px;
+          text-align: right;
+          vertical-align: top;
+          font-weight:bold;
+      }
+      #messages .message {
+          vertical-align: top;
+      }
+      #messages .line {
+          margin:0px;
+      }
+      #users {
+          width: 100%;
+          font-size: 12px;
+          color: #666666;
+          font-family: Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Arial, sans-serif;
+      }
+      #users .user {
+          text-align: left;
+          vertical-align: top;
+          font-weight:bold;
+      }
+      #users .timestamp {
+          text-align: right;
+          vertical-align: top;
+      }
+      #ichat {
+          width:70%;
+          border:1px solid black;
+          margin-right:15px;
+      }
+  </style>
+  <body style='margin:0px; padding:5px;'>
+    @output;literal@
+  </body>
+</html>
--- www/ajax/chat.tcl
+++ www/ajax/chat.tcl
@@ -18,7 +18,7 @@ if {$ban_p} {
 }
 
 set message_output ""
-set user_output "-"
+set user_output ""
 
 ::chat::Chat c1 -volatile -chat_id $id -session_id $s
 
@@ -42,30 +42,10 @@ switch -- $m {
   }
 }
 
-set output "
-<HTML>
-<style type='text/css'>
-#messages { font-size: 12px; color: #666666; font-family: Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Arial, sans-serif; }
-#messages .timestamp {vertical-align: top; color: #CCCCCC; }
-#messages .user {margin: 0px 5px; text-align: right; vertical-align: top; font-weight:bold;}
-#messages .message {vertical-align: top;}
-#messages .line {margin:0px;}
-#users { width: 100%; font-size: 12px; color: #666666; font-family: Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Arial, sans-serif; }
-#users .user {text-align: left; vertical-align: top; font-weight:bold; }
-#users .timestamp {text-align: right; vertical-align: top; }
-</style>
-<body style='margin:0px; padding:5px;'>"
-
+set output ""
 if { $message_output ne "" } {
-    append output "<div id='messages'>$message_output</div>\n"
+    append output "<div id='messages'>$message_output</div>"
 }
-
-if { $user_output ne "-" } {
-    append output "<table id='users'><tbody>$user_output</tbody></table>\n"
+if { $user_output ne "" } {
+    append output "<table id='users'><tbody>$user_output</tbody></table>"
 }
-
-append output "
-</body>
-</HTML>"
-
-ns_return 200 text/html $output
