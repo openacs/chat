98486d8951f484d08b56eb3fb17cfb77d4cbbf6f
Antonio Pisano <antonio@elettrotecnica.it>
2018-09-18 14:42:10 +0000

    Give templating a chance
    move all hardcoded styling to the adp template, which might be styled by the theme. Also, biring some order in the login proc, e.g. don't open and read a file to include a javascript all the time.

--- tcl/chat-ajax-procs.tcl
+++ tcl/chat-ajax-procs.tcl
@@ -41,69 +41,64 @@ namespace eval ::chat {
     return $result
   }
 
-  Chat proc login {-chat_id -package_id} {
-      auth::require_login
-      if {![info exists package_id]} {
-          set package_id [ad_conn package_id]
-      }
-      if {![info exists chat_id]} {
-          set chat_id $package_id
-      }
+    Chat proc login {-chat_id -package_id} {
+        auth::require_login
+        if {![info exists package_id]} {
+            set package_id [ad_conn package_id]
+        }
+        if {![info exists chat_id]} {
+            set chat_id $package_id
+        }
 
-      set context "id=$chat_id&s=[ad_conn session_id].[clock seconds]"
-      set jspath "packages/chat/www/ajax/chat.js"
+        set path [lindex [site_node::get_url_from_object_id -object_id $package_id] 0]
+        set base_url ${path}ajax/chat
+        template::head::add_javascript -src ${base_url}.js
+        set base_url [export_vars -base ${base_url} {{id $chat_id} {s "[ad_conn session_id].[clock seconds]"}}]
 
-      if { ![file exists [acs_root_dir]/$jspath] } {
-          return -code error "File [acs_root_dir]/$jspath does not exist"
-      }
-      set file [open [acs_root_dir]/$jspath]; set js [read $file]; close $file
+        set login_url [ns_quotehtml "${base_url}&m=login"]
+        set send_url  "${base_url}&m=add_msg&msg="
+        set users_url [ns_quotehtml "${base_url}&m=get_users"]
+        set html_url [ns_quotehtml [ad_conn url]?[ad_conn query]]
+        regsub {client=ajax} $html_url {client=html} html_url
 
-      set path      [lindex [site_node::get_url_from_object_id -object_id $package_id] 0]
-      set login_url [ns_quotehtml "${path}ajax/chat?m=login&$context"]
-      set send_url  "${path}ajax/chat?m=add_msg&$context&msg="
-      set users_url [ns_quotehtml "${path}ajax/chat?m=get_users&$context"]
-      set html_url [ns_quotehtml [ad_conn url]?[ad_conn query]]
-      regsub {client=ajax} $html_url {client=html} html_url
+        return [subst {
+          <script type='text/javascript' nonce='$::__csp_nonce'>
+              // register the data sources (for sending messages,
+              // receiving updates)
+              var pushMessage = registerDataConnection(pushReceiver, '${base_url}&m=get_new', false);
+              var pullUpdates = registerDataConnection(updateReceiver, '${base_url}&m=get_updates', true);
+          </script>
+          <form id='ichat_form' name='ichat_form' action='#'>
+            <iframe name='ichat' id='ichat' title='#chat.Conversation_area#'
+                    src='$login_url'>
+            </iframe>
+            <iframe name='ichat-users' id='ichat-users' title='#chat.Participants_list#'
+                    src='$users_url'>
+            </iframe>
+            <noframes>
+              <p>#chat.Your_browser_doesnt_support_#</p>
+              <p><a href='$html_url'>#chat.Go_to_html_version#</a></p>
+            </noframes>
+            <div id='ichat-message'>
+              #chat.message# <input tabindex='1' type='text' size='80' name='msg' id='chatMsg'>
+              <input type='submit' value='#chat.Send_Refresh#'>
+            </div>
+          </form>
 
-      return "\
-      <script type='text/javascript' nonce='$::__csp_nonce'>
-      $js
-      // register the data sources (for sending messages, receiving updates)
-      var pushMessage = registerDataConnection(pushReceiver, '$path/ajax/chat?m=get_new&$context', false);
-      var pullUpdates = registerDataConnection(updateReceiver, '$path/ajax/chat?m=get_updates&$context', true);
-      </script>
-      <form id='ichat_form' name='ichat_form' action='#'>
-      <iframe name='ichat' id='ichat' title='#chat.Conversation_area#'
-          frameborder='0' src='$login_url'
-          style='width:70%; border:1px solid black; margin-right:15px;' height='257'>
-      </iframe>
-      <iframe name='ichat-users' id='ichat-users' title='#chat.Participants_list#'
-          frameborder='0' src='$users_url'
-          style='width:25%; border:1px solid black;' height='257'>
-      </iframe>
-      <noframes>
-        <p>#chat.Your_browser_doesnt_support_#</p>
-        <p><a href='$html_url'>#chat.Go_to_html_version#</a></p>
-      </noframes>
-      <div style='margin-top:10px;'>
-      #chat.message# <input tabindex='1' type='text' size='80' name='msg' id='chatMsg'>
-      <input type='submit' value='#chat.Send_Refresh#'>
-      </div>
-      </form>
-
-      <script type='text/javascript' nonce='$::__csp_nonce'>
-      // Get a first update of users when the iframe is ready, then register a 5s interval to get new ones
-      document.getElementById('ichat-users').addEventListener('load', function (event) {
-          updateDataConnections();
-      }, false);
-      document.getElementById('ichat_form').addEventListener('submit', function (event) {
-          event.preventDefault();
-          pushMessage.chatSendMsg(\"$send_url\");
-      }, false);
-      var updateInterval = setInterval(updateDataConnections,5000);
-      </script>
-    "
-  }
+          <script type='text/javascript' nonce='$::__csp_nonce'>
+              // Get a first update of users when the iframe is ready,
+              // then register a 5s interval to get new ones
+              document.getElementById('ichat-users').addEventListener('load', function (event) {
+                  updateDataConnections();
+              }, false);
+              document.getElementById('ichat_form').addEventListener('submit', function (event) {
+                  event.preventDefault();
+                  pushMessage.chatSendMsg('$send_url');
+              }, false);
+              var updateInterval = setInterval(updateDataConnections,5000);
+          </script>
+        }]
+    }
 
     # if chat doesn't exist anymore, send a message that will inform
     # the user of being looking at an invalid chat
--- www/ajax-chat-script.adp
+++ www/ajax-chat-script.adp
@@ -3,6 +3,23 @@
 <property name="&doc">doc</property>
 <property name="focus">ichat_form.msg</property>
 
+<style type='text/css'>
+   #ichat {
+       width:70%;
+       height:257px;
+       border:1px solid black;
+       margin-right:15px;
+   }
+   #ichat-users {
+       height:257px;
+       width:25%;
+       border:1px solid black;
+   }
+   #ichat-message {
+       margin-top:10px;
+   }
+</style>
+
 <h1>@doc.title@</h1>
 <p>
 <a href="room-exit?room_id=@room_id@" class="button" title="#chat.exit_msg#">#chat.Log_off#</a> 
