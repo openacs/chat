8f0b64e6835cce62b5a90d1aa4487679b43553ae
Antonio Pisano <antonio@elettrotecnica.it>
2019-03-07 14:00:04 +0000

    Maintenance upgrade to get rid of obsolete package parameters

---
+++ tcl/apm-callback-procs.tcl
@@ -0,0 +1,42 @@
+ad_library {
+
+    APM Callback procs
+
+}
+
+namespace eval chat::apm {}
+
+ad_proc -private chat::apm::upgrade {
+    -from_version_name
+    -to_version_name
+} {
+    Upgrade logics for the chat package
+} {
+    apm_upgrade_logic \
+	-from_version_name $from_version_name \
+	-to_version_name $to_version_name \
+	-spec {
+	    6.0.0d3 6.0.0d4 {
+                ns_write "Deleting obsolete parameters"
+                set parameter_names {
+                    AppletHeight
+                    AppletWidth
+                    DefaultClient
+                    ServerHost
+                    ServerPort
+                    ShowAvatarP
+                }
+                ::xo::dc foreach [subst {
+                    select parameter_id, parameter_name
+                      from apm_parameters
+                    where package_key = 'chat'
+                      and parameter_name in ('[join $parameter_names ']')
+                }] {
+                    apm_parameter_unregister \
+                        -package_key chat $parameter_id
+                    ns_write " - deleting obsolete parameter chat.$parameter_name"
+                }
+                ns_write "Finished!"
+            }
+        }
+}
